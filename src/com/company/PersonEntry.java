659c5b46b308ff65e5c1fee87fe2e5d48855af66
NM17 <nada.mahmoud@detroitlabs.com>
2019-01-28 19:12:15 -0500

    creating personEntry class and getter/setter

---
+++ src/com/company/PersonEntry.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class PersonEntry {
+    private String name;
+    private int phoneNumber;
+    private String address;
+    private int birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+    public PersonEntry() {
+        this.name;
+        this.
+    }
+
+}
